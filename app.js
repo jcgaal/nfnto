edd4cb8634b839e1b6c93a69a1c0f705e6c71a01
jcgaal <hello@jcgaal.com>
2016-12-14 23:10:15 -0600

    20161214

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('nfnto')
+  res.send('nfnto 2016')
 })
 
 app.get('/about', function (req, res) {
